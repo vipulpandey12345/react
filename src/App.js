e1fcd2af619c7d0e4eea9fa1681cf35604b8a8a4
Vipul Pandey <54221581+vipulpandey12345@users.noreply.github.com>
2023-08-20 21:02:54 -0700

    new commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 
 import './App.css';
 import Employee from './components/Employee'
+import { useState } from 'react';
 function App() {
+  const showEmployees = true;
+  const [role, setRole] = useState('dev');
   return (
-    <Employee/>
+    <div className='App'>
+      {
+        showEmployees ?(
+          <>
+          <input
+          type = 'text'
+          onChange={(e) => setRole(e.target.value)}
+          />
+          <Employee name = "Caleb" role = {role}/>
+          <Employee name = "Valerie" role = 'nurse'/>
+          </>
+        )
+      :(
+        <p>Hi</p>
+      )}
+    </div>
   );
 }
 
