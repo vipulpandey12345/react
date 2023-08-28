83884dfe7c4323e4c44df31b8bdbfe6bac8d6b69
Vipul Pandey <54221581+vipulpandey12345@users.noreply.github.com>
2023-08-27 20:18:26 -0700

    newest commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 
 import './App.css';
+import {v4 as uuidv4} from 'uuid';
 import Employee from './components/Employee'
 import { useState } from 'react';
 function App() {
   const showEmployees = true;
   const [role, setRole] = useState('dev');
+  const [employees, setEmployees] = useState(
+    [
+    {name: 'Caleb',
+    role: 'Youtube Sensation',
+    img: 'https://images.pexels.com/photos/17993439/pexels-photo-17993439/free-photo-of-wood-bench-light-city.jpeg'
+    },
+    {
+    name: 'Valerie',
+    role: 'Developer',
+    img: 'https://images.pexels.com/photos/17993439/pexels-photo-17993439/free-photo-of-wood-bench-light-city.jpeg'
+    },
+    {
+      name: 'Jane',
+      role: 'Stay at home mom',
+      img: 'https://images.pexels.com/photos/17993439/pexels-photo-17993439/free-photo-of-wood-bench-light-city.jpeg'
+    }
+    ]
+  );
   return (
-    <div className='App'>
+    <div className='App bg-red-300'>
       {
         showEmployees ?(
           <>
@@ -14,8 +33,17 @@ function App() {
           type = 'text'
           onChange={(e) => setRole(e.target.value)}
           />
-          <Employee name = "Caleb" role = {role}/>
-          <Employee name = "Valerie" role = 'nurse'/>
+          <div className='flex flex-wrap'>
+            {employees.map((employee) => {
+              console.log(uuidv4())
+              return(<Employee
+                key = {uuidv4()}
+                name = {employee.name} 
+                role = {employee.role} 
+                img = {employee.img}/>
+                );
+            })}
+          </div>
           </>
         )
       :(
